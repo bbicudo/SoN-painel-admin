481ef01141fcf9d8032ce96739141940ab4421f5
bbicudo <brunobranco4@hotmail.com>
2021-11-11 17:02:07 -0300

    Criação de estrutura de navegação

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 $path = $_SERVER['PATH_INFO'] ?? '/';
 
 if($path == '/'){
-    require __DIR__ . '/routes.php';
+    require __DIR__ . '/site/routes.php';
 }else{
     echo 'Página não encontrada!';
 }
