fee2edf975fbb091b369d234157ad017887c331b
bbicudo <brunobranco4@hotmail.com>
2021-11-17 15:17:06 -0300

    Criação de templates e implantação de editor de texto (trix)

--- templates/admin/pages/create.tpl.php
+++ templates/admin/pages/create.tpl.php
@@ -1,5 +1,26 @@
 <h3>Administração de Páginas</h3>
 
-//criação de página
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" type="text" class="form-control" placeholder="Título da Página...">
+    </div>
 
-<a href="/admin">Home</a>
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input name="url" id="pagesUrl" type="text" class="form-control" placeholder="URL amigável, deixe em branco para informar a página inicial...">
+        </div>
+    </div>
+
+    <div class="form-group">
+        //Editor de Texto
+    </div>
+
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
+<hr>
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
