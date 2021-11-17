9523c6821a285be3e6d2d1cf07073290b9e9484d
bbicudo <brunobranco4@hotmail.com>
2021-11-17 17:23:58 -0300

    Inicio de integração com banco de dados

---
+++ admin/pages/db.php
@@ -0,0 +1,21 @@
+<?php
+
+$pages_all = function(){
+    //buscar todas as páginas
+};
+
+$pages_one = function($id){
+    //buscar uma única página
+};
+
+$pages_create = function(){
+    //cadastra uma página
+};
+
+$pages_edit = function($id){
+    //edita uma página
+};
+
+$pages_delete = function($id){
+    //deleta uma página
+};
